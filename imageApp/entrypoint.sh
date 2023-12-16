eaf7fbf6bae06f9b87d29b3a0389c9b71ecf69da
Fanuel26 <56047301+fanuel26@users.noreply.github.com>
2023-12-16 14:01:05 +0000

    cc

--- imageApp/entrypoint.sh
+++ imageApp/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Créer un lien symbolique vers le volume dans un répertoire local
-ln -s /app/public/uploads /chemin/sur/hote
+ln -s /app/public/uploads /home/uploads/data
 
 # Démarrer votre application (remplacez ceci par la commande réelle pour démarrer votre application)
 exec "$@"
